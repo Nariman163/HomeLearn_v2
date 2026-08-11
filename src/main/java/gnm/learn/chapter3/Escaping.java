636fd33752f7d1576fe1b5d9cb51ea61dcfabadc
Nariman <nariman@MacBook-Pro-Nariman.local>
2026-08-11 23:25:11 +0300

    Escaping learn

---
+++ src/main/java/gnm/learn/chapter3/Escaping.java
@@ -0,0 +1,8 @@
+package gnm.learn.chapter3;
+
+public class Escaping {
+    public static void main(String[] args) {
+        System.out.println("\\");
+        System.out.println("Test varm\" my office\'");
+    }
+}
---
+++ src/main/java/gnm/learn/logicTasks/IsPrime.java
@@ -0,0 +1,10 @@
+package gnm.learn.logicTasks;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        //Напишите программу , находящую все простые числа от 2 до 100.
+    int i,j;
+    boolean isprime;
+
+    }
+}
